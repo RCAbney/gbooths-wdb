e1c8e7a824f0aaff63817545adb05467db2678f7
RCAbney <ryan@rcabney.com>
2025-03-02 22:52:07 -0500

    moar mobile stuffs

--- src/components/NoBooths.jsx
+++ src/components/NoBooths.jsx
@@ -5,9 +5,9 @@ function NoBooths() {
     return (
         <div className="p-4 flex items-center justify-center h-[calc(100vh-70px)]">
             <Link
-                type="button"
-                className="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 to="/all-booths"
+                className="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 touch-manipulation"
+                style={{ minHeight: '44px' }}
             >
                 <svg
                     className="mx-auto h-12 w-12 text-gray-400"
